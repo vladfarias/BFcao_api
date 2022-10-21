4ce71bf7726e3502c3b44ed26d72690c84e8946f
Vladfarias <vladfarias@gmail.com>
2022-10-21 06:23:53 -0300

    creating a connection with Mongodb

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const dotenv = require('dotenv');
 dotenv.config({path:'./.env'});
 
 const app = require('./app');
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', 
+    process.env.DATABASE_PASSWORD)
 
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then((con)=>{
+    console.log(con.connection);
+    console.log('DB conection sucessfull!');
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, ()=>{
